092fa17a83f1a7b676f346ecdc4751ceb15f9290
petes05 <peterwsimpson@gmail.com>
2019-05-15 21:50:11 +0100

    Fix tab format

--- vue-app/petesaddressbook/src/store.js
+++ vue-app/petesaddressbook/src/store.js
@@ -5,31 +5,31 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-		// list of contacts
-		contacts: [],
-		// id of selected contact
-		selectedContact: undefined,
-		// true if a contact is selected
-		contactSelected: false
-	},
+    // list of contacts
+    contacts: [],
+    // id of selected contact
+    selectedContact: undefined,
+    // true if a contact is selected
+    contactSelected: false
+  },
   mutations: {
-		updateContacts(state, contacts) {
-			state.contacts = contacts;
-		},
-		addContactToList(state, contact) {
-			state.contacts.push(contact);
-		},
-		updateContactName(state, updatedContact) {
-			let contactToUpdate = state.contacts.find( contact => contact.id === updatedContact.id );
-			contactToUpdate.first_names = updatedContact.first_names;
-			contactToUpdate.last_name = updatedContact.last_name;
-		},
-		updateSelectedContact(state, contact) {
-			state.selectedContact = contact;
-		},
-		updateContactSelected(state, bool) {
-			state.contactSelected = bool;
-		}
-	},
+    updateContacts(state, contacts) {
+      state.contacts = contacts;
+    },
+    addContactToList(state, contact) {
+      state.contacts.push(contact);
+    },
+    updateContactName(state, updatedContact) {
+      let contactToUpdate = state.contacts.find( contact => contact.id === updatedContact.id );
+      contactToUpdate.first_names = updatedContact.first_names;
+      contactToUpdate.last_name = updatedContact.last_name;
+    },
+    updateSelectedContact(state, contact) {
+      state.selectedContact = contact;
+    },
+    updateContactSelected(state, bool) {
+      state.contactSelected = bool;
+    }
+  },
   actions: {}
 });
